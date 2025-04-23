f75db0a380ba39e6e4176b4dfeff86c3fd9c0652
Denis Lipatov <nutela113@yandex.ru>
2025-04-24 00:23:25 +0300

    Fixed

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,80 +1,79 @@
 package ru.netology;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 
 public class Main {
 
-    public static final String letters = "abc";
-    public static final int textLength = 100_000;
-    public static String maxA = "";
-    public static int maxCountA = -1;
-    public static String maxB = "";
-    public static int maxCountB = -1;
-    public static String maxC = "";
-    public static int maxCountC = -1;
+    public static final String LETTERS = "abc";
+    public static final int SIZE = 100;
+    public static final int TEXT_LENGTH = 100_000;
+    public static final int QUANTITY = 10_000;
 
-    public static ArrayBlockingQueue<String> queueA = new ArrayBlockingQueue<>(100);
-    public static ArrayBlockingQueue<String> queueB = new ArrayBlockingQueue<>(100);
-    public static ArrayBlockingQueue<String> queueC = new ArrayBlockingQueue<>(100);
+    public static final List<BlockingQueue<String>> queues = Arrays.asList(
+            new ArrayBlockingQueue<>(SIZE),
+            new ArrayBlockingQueue<>(SIZE),
+            new ArrayBlockingQueue<>(SIZE)
+    );
 
     public static void main(String[] args) throws InterruptedException {
         new Thread(() -> {
-            String text = generateText(letters, textLength);
-            try {
-                queueA.put(text);
-                queueB.put(text);
-                queueC.put(text);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+            for (int i = 0; i < QUANTITY; i++) {
+                String text = generateText(LETTERS, TEXT_LENGTH);
+                try {
+                    for (BlockingQueue<String> queue : queues) {
+                        queue.put(text);
+                    }
+                } catch (InterruptedException e) {
+                    return;
+                }
             }
         }).start();
 
-        Thread threadA = analyzeA(queueA.take());
-        Thread threadB = analyzeB(queueB.take());
-        Thread threadC = analyzeC(queueC.take());
+        List<Thread> consumers = new ArrayList<>();
+        for (int i = 0; i < queues.size(); i++) {
+            Thread consumer = getConsumer(i);
+            consumers.add(consumer);
+        }
 
-        threadA.start();
-        threadB.start();
-        threadC.start();
+        for (Thread consumer : consumers) {
+            consumer.join();
+        }
 
-        threadA.join();
-        threadB.join();
-        threadC.join();
-
-        System.out.println("С символом a: " + maxCountA + maxA);
-        System.out.println("С символом b: " + maxCountB + maxB);
-        System.out.println("С символом c: " + maxCountC + maxC);
     }
 
-    public static Thread analyzeA(String text) {
-        return new Thread(() -> {
-            int count = (int) text.chars().filter(x -> (char) x == 'a').count();
-            if (count > maxCountA) {
-                maxA = text;
-                maxCountA = count;
+    private static Thread getConsumer(int i) {
+        char c = (char) ('a' + i);
+
+        Thread consumer = new Thread(() -> {
+            BlockingQueue<String> queue = queues.get(i);
+            String maxString = "";
+            long maxCount = 0;
+            for (int j = 0; j < QUANTITY; j++) {
+                try {
+                    String s = queue.take();
+                    long count = countChar(s, c);
+                    if (count > maxCount) {
+                        maxCount = count;
+                        maxString = s;
+                    }
+                } catch (InterruptedException e) {
+                    return;
+                }
             }
+            System.out.printf("Max count of letter %c - %d - found in the following string: " +
+                    "%s\n", c, maxCount, maxString.substring(0, 75) + "...");
         });
+        consumer.start();
+        return consumer;
     }
 
-    public static Thread analyzeB(String text) {
-        return new Thread(() -> {
-            int count = (int) text.chars().filter(x -> (char) x == 'b').count();
-            if (count > maxCountB) {
-                maxB = text;
-                maxCountB = count;
-            }
-        });
-    }
-
-    public static Thread analyzeC(String text) {
-        return new Thread(() -> {
-            int count = (int) text.chars().filter(x -> (char) x == 'c').count();
-            if (count > maxCountC) {
-                maxC = text;
-                maxCountC = count;
-            }
-        });
+    public static int countChar(String text, char c) {
+        return (int) text.chars().filter(x -> x == c).count();
     }
 
     public static String generateText(String letters, int length) {
